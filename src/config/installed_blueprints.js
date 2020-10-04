fa94bfea098bcb2c8bfaeb6340f03e94137e809e
Crowdbotics <team@crowdbotics.com>
2020-10-04 21:36:39 +0000

    add package blueprint-react-native-emailauth-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'EmailAuth125732', human_name: 'Email Auth', access_route: 'EmailAuth125732', icon: 'envelope-o'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import EmailAuth125732Reducer from '../features/EmailAuth125732/redux/reducers';
 
 export const combinedReducers = combineReducers({
   blank: (state, action) => {
@@ -15,5 +16,6 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+EmailAuth125732: EmailAuth125732Reducer,
 
 });
